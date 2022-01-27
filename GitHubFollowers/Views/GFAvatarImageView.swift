29db8483abee659e6ce7e704a011c61c72ab2453
Raul Rodriguez III <hi@raulrodrigueziii.com>
2022-01-26 21:29:31 -0600

    Creating Custom Follower UICollectionViewCell

---
+++ GitHubFollowers/Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Raul Rodriguez III on 1/26/22.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+}
