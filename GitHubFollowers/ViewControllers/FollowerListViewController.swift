5a5c5a0fc7bab2fa41f652697ff3e42dac905105
Raul Rodriguez III <hi@raulrodrigueziii.com>
2022-01-27 21:48:12 -0600

    Creating Custom UICollectionViewLayout

--- GitHubFollowers/ViewControllers/FollowerListViewController.swift
+++ GitHubFollowers/ViewControllers/FollowerListViewController.swift
@@ -36,11 +36,28 @@ class FollowerListViewController: UIViewController {
     func configureCollectionView() {
         
         // Initializing UICollectionView
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewLayout {
+        
+        let width = view.bounds.width
+        let padding: CGFloat =  12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+        
     }
     
     
